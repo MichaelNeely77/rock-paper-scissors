eafcef6f80d3dde30cf1acd7d254e83758cb2878
Michael Neely <michael@michaelneely.net>
2023-01-29 18:35:10 -0500

    Added teh logic for the game

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS'
+let gameIsRunning = false;
 const getPlayerChoice = function() {
     const selection = prompt(`${ROCK}, ${PAPER}, or ${SCISSORS}?`, '').toUpperCase();
     if(selection !== ROCK &&
@@ -13,8 +17,32 @@ const getPlayerChoice = function() {
         return DEFAULT_USER_CHOICE
     }
     return selection;
-}
+};
 
+const getComputerChoice = function() {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+};
+
+const getWinner = function(cChoice, pChoice) {
+    if (pChoice === cChoice) {
+        return RESULT_DRAW;
+    } else if (
+        cChoice === ROCK && pChoice === PAPER ||
+        cChoice === PAPER && pChoice === SCISSORS ||
+        cChoice === SCISSORS && pChoice ===ROCK
+    ){
+        return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
+}
 
 startGameBtn.addEventListener('click', function() {
     if(gameIsRunning) {
@@ -23,5 +51,7 @@ startGameBtn.addEventListener('click', function() {
     gameIsRunning = true;
     console.log('Game is starting...');
     const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(computerChoice, playerSelection);
+    console.log(winner);
 });
