2337cf3c41d8e90294fb6a097278756f2ea0e52d
Michael Neely <michael@michaelneely.net>
2023-02-01 17:33:44 -0500

    More on functions

--- app.js
+++ app.js
@@ -82,3 +82,33 @@ startGameBtn.addEventListener('click', () => {
     alert(message);
     gameIsRunning = false;
 });
+
+// not related to the game
+
+const sumUp = (resultHandler, ...numbers) => {
+    const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+    };
+
+    let sum = 0;
+    for (const num of numbers) {
+        sum += validateNumber(num);
+    }
+    resultHandler(sum);
+};
+
+const subtractUp = function (resultHandler) {
+    let sum = 0;
+    for (const num of arguments) { //don't use that
+        sum -= num;
+    }
+    return sum;
+};
+
+const showResult = (result) => {
+    alert('The sum of adding all the numbers is ' + result);
+};
+
+sumUp(showResult, 5, 10, -3, 6, 10, 46);
+sumUp(showResult, 5, 10, -3, 6, 10, 25, 68);
+subtractUp(showResult, 5, 10, -3, 6, 10, 25, 68);
